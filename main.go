049d247b24ea137cac980333d7309b6ea8af8704
daseinsucks <alexstepov97@gmail.com>
2022-10-03 20:15:48 +0300

    setup sequence chat finished

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"math/big"
+	"strconv"
 
 	//"math"
 	"os"
@@ -24,6 +25,15 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+var numericKeyboard = tgbotapi.NewReplyKeyboard(
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("ERC20"),
+		tgbotapi.NewKeyboardButton("ERC20Snapshot"),
+	),
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("ERC721")),
+)
+
 var mainKeyboard = tgbotapi.NewReplyKeyboard(
 	tgbotapi.NewKeyboardButtonRow(
 		tgbotapi.NewKeyboardButton("Verify personal wallet")),
@@ -40,7 +50,21 @@ type user struct {
 	dialog_status int64
 	setup_status  int64
 	repo          string
+	dao           string
+	votingtype    int64
+	vtt           string
 }
+
+var baseURL = "http://localhost:3000/dao"
+
+var user_id_query = "?user_id="
+var chat_query = "&chat_id="
+var address_query = "&address="
+var type_query = "&votingtype="
+var contract_query = "&votingtokencontract="
+var name_query = "&daoname="
+
+//localhost:3000/dao?user_id=1337&chat_id=1337&address=23746624386&votingtype=1&votingtokencontract=3278465ASDW23&daoname=lol
 
 //main database for dialogs, key (int64) is telegram user id
 var userDatabase = make(map[int64]user) // consider to change in persistend data storage?
@@ -99,19 +123,18 @@ func main() {
 	u.Timeout = 60
 
 	updates := bot.GetUpdatesChan(u)
-
+	//TODO: add check tgid == daoaddress(tgid)
 	//whenever bot gets a new message, check for user id in the database happens, if it's a new user, the entry in the database is created.
 	for update := range updates {
 
 		if update.Message != nil {
 			if _, ok := userDatabase[update.Message.From.ID]; !ok {
-				userDatabase[update.Message.From.ID] = user{update.Message.Chat.ID, update.Message.Chat.UserName, 0, 0, "0"}
+				userDatabase[update.Message.From.ID] = user{update.Message.Chat.ID, update.Message.Chat.UserName, 0, 0, "0", "0", 0, "0"}
 
 				isRegistered := checkDao(auth, union, update.Message.Chat.ID)
 				if isRegistered {
 					if updateDb, ok := userDatabase[update.Message.From.ID]; ok {
 						updateDb.dialog_status = 1
-
 						userDatabase[update.Message.From.ID] = updateDb
 					}
 				} else {
@@ -126,27 +149,97 @@ func main() {
 
 			} else {
 				switch userDatabase[update.Message.From.ID].setup_status {
+
 				case 1:
 					if updateDb, ok := userDatabase[update.Message.From.ID]; ok {
 						updateDb.repo = update.Message.Text
 						updateDb.setup_status = 2
 						userDatabase[update.Message.From.ID] = updateDb
-						chat := update.Message.Chat
-						user := update.Message.From
-						chatmember, err := bot.GetChatMember(tgbotapi.ChatConfigWithUser{
-							ChatID:             chat.ID,
-							SuperGroupUsername: chat.ChatConfig().SuperGroupUsername,
-							UserID:             user.ID,
-						})
-
+						chatvar := update.Message.Chat
+						uservar := update.Message.From
+						isAdmin := checkAdmin(chatvar, uservar)
+						if !isAdmin {
+							msg := tgbotapi.NewMessage(userDatabase[update.Message.From.ID].tgid, "Sorry, but only admin of the chat may connect it to the DAO!")
+							bot.Send(msg)
+							delete(userDatabase, update.Message.From.ID)
+						}
 						isUserRegistered := checkUser(auth, passport, update.Message.From.ID)
-						if isUserRegistered {
-
+						if !isUserRegistered {
+							msg := tgbotapi.NewMessage(userDatabase[update.Message.From.ID].tgid, "Sorry, but before attaching DAO you should apply for passport here:")
+							bot.Send(msg)
+							delete(userDatabase, update.Message.From.ID)
+						}
+						if isUserRegistered && isAdmin {
+							msg := tgbotapi.NewMessage(userDatabase[update.Message.From.ID].tgid, "Okay, tell me your Multisig address!")
+							bot.Send(msg)
 						}
 					}
+				case 2:
+					if updateDb, ok := userDatabase[update.Message.From.ID]; ok {
+						updateDb.dao = update.Message.Text
+						updateDb.setup_status = 3
+						userDatabase[update.Message.From.ID] = updateDb
+						msg := tgbotapi.NewMessage(userDatabase[update.Message.From.ID].tgid, "Cool, now I need to know your voting token's type")
+						msg.ReplyMarkup = numericKeyboard
+						bot.Send(msg)
+					}
+				case 3:
+					if update.Message.Text == "ERC20Snapshot" || update.Message.Text == "ERC20" || update.Message.Text == "ERC721" {
 
+						var tokenType int64
+						if update.Message.Text == "ERC20" {
+							tokenType = 0
+						} else if update.Message.Text == "ERC20Snapshot" {
+							tokenType = 1
+						} else if update.Message.Text == "ERC20Votes" {
+							tokenType = 2
+						}
+
+						if updateDb, ok := userDatabase[update.Message.From.ID]; ok {
+							updateDb.votingtype = tokenType
+							updateDb.setup_status = 4
+							userDatabase[update.Message.From.ID] = updateDb
+							msg := tgbotapi.NewMessage(userDatabase[update.Message.From.ID].tgid, "Okay, last question: what's the address of your voting token?")
+							msg.ReplyMarkup = tgbotapi.NewRemoveKeyboard(true)
+							bot.Send(msg)
+						}
+
+					} else {
+						msg := tgbotapi.NewMessage(update.Message.Chat.ID, "That's not the type!")
+						bot.Send(msg)
+					}
+
+				case 4:
+					if updateDb, ok := userDatabase[update.Message.From.ID]; ok {
+						updateDb.vtt = update.Message.Text
+						updateDb.setup_status = 4
+						userDatabase[update.Message.From.ID] = updateDb
+
+						msg := tgbotapi.NewMessage(userDatabase[update.Message.From.ID].tgid, "Alright, now apply for union here:")
+						bot.Send(msg)
+
+						userIDint := userDatabase[update.Message.From.ID].tgid
+						userID := strconv.FormatInt(userIDint, 10)
+
+						chatIDint := update.Message.Chat.ID
+						chatID := strconv.FormatInt(chatIDint, 10)
+
+						address := userDatabase[update.Message.From.ID].dao
+
+						votingTypeint := userDatabase[update.Message.From.ID].votingtype
+						votingType := strconv.FormatInt(votingTypeint, 10)
+
+						vtt := userDatabase[update.Message.From.ID].vtt
+						chatName := update.Message.Chat.UserName
+
+						link := baseURL + user_id_query + userID + chat_query + chatID + address_query + address + type_query + votingType + contract_query + vtt + name_query + chatName
+
+						msg2 := tgbotapi.NewMessage(userDatabase[update.Message.From.ID].tgid, link)
+						bot.Send(msg2)
+					}
 				}
 			}
+
 		} else if userDatabase[update.Message.From.ID].setup_status == 0 {
 
 			switch userDatabase[update.Message.From.ID].dialog_status {
@@ -173,7 +266,6 @@ func main() {
 }
 
 func checkDao(auth *bind.TransactOpts, pc *union.UnionCaller, tgid int64) bool {
-
 	registration, err := pc.DaoAddresses(&bind.CallOpts{
 		From:    auth.From,
 		Context: context.Background(),
@@ -193,11 +285,10 @@ func checkDao(auth *bind.TransactOpts, pc *union.UnionCaller, tgid int64) bool {
 }
 
 func checkUser(auth *bind.TransactOpts, pc *passport.PassportCaller, tgid int64) bool {
-	a := big.NewInt(tgid)
 	registration, err := pc.TgIdToAddress(&bind.CallOpts{
 		From:    auth.From,
 		Context: context.Background(),
-	}, a)
+	}, tgid)
 
 	log.Println(registration)
 
@@ -209,5 +300,21 @@ func checkUser(auth *bind.TransactOpts, pc *passport.PassportCaller, tgid int64)
 		return false
 	} else {
 		return true
+	}
+}
+
+func checkAdmin(chat *tgbotapi.Chat, user *tgbotapi.User) bool {
+	thing := tgbotapi.ChatConfigWithUser{
+		ChatID:             chat.ID,
+		SuperGroupUsername: chat.ChatConfig().SuperGroupUsername,
+		UserID:             user.ID,
+	}
+	thing2 := tgbotapi.GetChatMemberConfig{thing}
+
+	chatmember, _ := bot.GetChatMember(thing2)
+	if chatmember.Status == "administrator" || chatmember.Status == "creator" {
+		return true
+	} else {
+		return false
 	}
 }
